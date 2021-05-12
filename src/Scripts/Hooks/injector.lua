90e21fad4d378587188b81b7640f27a19175df49
DasCleverle <dascleverle@gmail.com>
2021-05-12 10:56:18 +0200

    Add 'hook.lua' file to be injected to hook env

--- src/Scripts/Hooks/injector.lua
+++ src/Scripts/Hooks/injector.lua
@@ -15,6 +15,12 @@ end
 
 --- FUNCTIONS ---
 
+local function loadHook(path, dir)
+    local env = setmetatable({}, { __index = _G })
+    env.package.path = dir .. "/?.lua;" .. package.path;
+    return pcall(setfenv(loadfile(path), env))
+end
+
 local function loadFile(path, dir)
     local file = io.open(path, "r")
     local content = file:read("*a")
@@ -27,27 +33,36 @@ local function loadFile(path, dir)
     return result, success
 end
 
+local function logResult(success, result)
+    if success then
+        logInfo("File was loaded successfully")
+    else
+        logInfo("File could not be loaded: " .. tostring(result))
+    end
+end
+
 --- LOAD FILES ---
 
 local loaders = {}
 local sourceDir = writedir .. "Scripts/Inject"
 
-local function loadFiles() 
+local function loadFiles()
     for entry in lfs.dir(sourceDir) do
         local fullPath = sourceDir .. "/" .. entry
 
         if lfs.attributes(fullPath, "mode") == "directory" and entry ~= ".." and entry ~= "." then
             local initLua = fullPath .. "/init.lua"
+            local hookLua = fullPath .. "/hook.lua"
 
             loaders[entry] = function()
-                logInfo("Loading " .. initLua .. " into mission environment")
+                if lfs.attributes(hookLua) ~= nil then
+                    logInfo("Loading " .. hookLua .. " into hook environment")
+                    logResult(loadHook(hookLua, fullPath))
+                end
 
-                local result, success = loadFile(initLua, fullPath)
-
-                if success then
-                    logInfo("File was loaded successfully")
-                else
-                    logInfo("File load result: " .. tostring(result) .. ", success: " .. tostring(success))
+                if lfs.attributes(initLua) ~= nil then
+                    logInfo("Loading " .. initLua .. " into mission environment")
+                    logResult(loadFile(initLua, fullPath))
                 end
             end
         end
@@ -63,10 +78,6 @@ end
 local callbacks = {}
 
 function callbacks.onSimulationStart()
-	net.dostring_in("mission", "a_do_script('env.mission.name = \"".. DCS.getMissionName() .. "\"')")
-
-    logInfo("Handling onSimulationStart")
-
     for name, loader in pairs(loaders) do
         logInfo("Loading module " .. name)
         loader()
